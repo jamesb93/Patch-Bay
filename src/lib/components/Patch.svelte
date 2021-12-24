64c4fc9f0224bcecee492c4e800e70897244a969
James Bradbury <me@jamesbradbury.net>
2021-12-24 13:54:44 +0000

    make patch responsive

--- src/lib/components/Patch.svelte
+++ src/lib/components/Patch.svelte
@@ -7,6 +7,12 @@
 	export let id = '';
 	export let limit = 60;
 
+	const preview = patch.slice(0, limit)
+		.replace('<code>', '')
+		.replace('</code>', '')
+		.replace('<pre>', '')
+		.replace('</pre>', '')
+
 	const copyPatch = () => {
 		if (browser) {
 			navigator.clipboard.writeText(patch);
@@ -18,7 +24,6 @@
 			navigator.clipboard.writeText(`https://shareapatchwith.me/patch/${id}`);
 		}
 	};
-	
 </script>
 
 <div class="patch">
@@ -29,18 +34,21 @@
 			<button on:click={copyLink}>copy link</button>
 		</div>
 	</div>
-	<code class="code">{patch.slice(0, limit)}</code>
-	<div class="description">{description || 'untitled'}</div>
+	<div class="code">
+		{preview}
+	</div>
+	<div class="description">{description || 'no description'}</div>
 </div>
 
 <style>
 	.patch {
-		position: relative;
-		display: flex;
-		flex-direction: column;
-		gap: 0.5em;
+		display: grid;
+		grid-template-rows: auto auto auto;
+		gap: 0.3em;
 		padding: 0.5em;
 		border: 1px solid grey;
+		margin: 0 auto;
+		min-width: 100%;
 	}
 
 	.top {
@@ -56,9 +64,5 @@
 
 	.code {
 		color: grey;
-	}
-
-	.copy {
-		width: max-content;
 	}
 </style>
