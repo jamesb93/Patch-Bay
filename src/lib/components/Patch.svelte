fdfdc4cbd8837d2072ff35231c3e0ff3005a37f0
James Bradbury <me@jamesbradbury.net>
2021-12-24 00:09:30 +0000

    style the patch component for more varied use

--- src/lib/components/Patch.svelte
+++ src/lib/components/Patch.svelte
@@ -1,25 +1,35 @@
 <script>
-	import { goto } from '$app/navigation';
 	import { browser } from '$app/env';
 
 	export let name = '';
 	export let patch = '';
 	export let description = '';
 	export let id = '';
+	export let limit = 60;
 
 	const copyPatch = () => {
 		if (browser) {
 			navigator.clipboard.writeText(patch);
 		}
 	};
+
+	const copyLink = () => {
+		if (browser) {
+			navigator.clipboard.writeText(`https://shareapatchwith.me/patch/${id}`);
+		}
+	};
+	
 </script>
 
 <div class="patch">
 	<div class="top">
 		<a href={`/patch/${id}`} class="name">{name || 'untitled'}</a>
-		<button on:click={copyPatch} class="copy">copy</button>
+		<div class="links">
+			<button on:click={copyPatch} class="copy">copy text</button>
+			<button on:click={copyLink}>copy link</button>
+		</div>
 	</div>
-	<code class="code">{patch.slice(0, 60)}</code>
+	<code class="code">{patch.slice(0, limit)}</code>
 	<div class="description">{description || 'untitled'}</div>
 </div>
 
@@ -28,8 +38,8 @@
 		position: relative;
 		display: flex;
 		flex-direction: column;
-		gap: 1em;
-		padding: 1em;
+		gap: 0.5em;
+		padding: 0.5em;
 		border: 1px solid grey;
 	}
 
@@ -42,7 +52,6 @@
 	.name {
 		font-weight: bold;
 		text-decoration: underline;
-		font-size: 1.1rem;
 	}
 
 	.code {
