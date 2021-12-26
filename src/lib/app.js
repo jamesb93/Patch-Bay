bf20a23629ca25a9964ae79cc570a4a0d7526948
James Bradbury <me@jamesbradbury.net>
2021-12-26 15:14:20 +0000

    format app.js

--- src/lib/app.js
+++ src/lib/app.js
@@ -3,12 +3,12 @@ import { onAuthStateChanged, getAuth } from 'firebase/auth';
 import { writable } from 'svelte/store';
 
 const firebaseConfig = {
-	"apiKey": import.meta.env.VITE_FIREBASE_APIKEY,
-	"authDomain": import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	"projectId": import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	"storageBucket": import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	"messagingSenderId": import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	"appId": import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+	apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
