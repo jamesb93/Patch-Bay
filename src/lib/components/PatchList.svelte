9aee745b6b2a0cdcb3828721f6925043c4533121
James Bradbury <me@jamesbradbury.net>
2021-12-26 15:13:57 +0000

    a component to represent lists of patches

---
+++ src/lib/components/PatchList.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { PatchObject } from '$lib/types';
+	import Patch from '$lib/components/Patch.svelte';
+
+	export let patches: Array<PatchObject> = [];
+</script>
+
+<div class="patchList">
+	{#each patches as obj}
+		<Patch
+			name={obj.data.name}
+			patch={obj.data.patch}
+			description={obj.data.description}
+			id={obj.id}
+		/>
+	{/each}
+</div>
+
+<style>
+	.patchList {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+	}
+</style>
